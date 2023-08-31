788b91105eeb50c401d73a229a53df144c6f09f9
fizzahkhan123 <guysr92@gmail.com>
2023-08-31 00:13:01 -0700

    product page complete and loader added

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost: ${port}`);
-  console.log('env variables', process.env);
+  console.log(`Example app listening on port http://localhost:${port}`);
+  // console.log('env variables', process.env);
 });
